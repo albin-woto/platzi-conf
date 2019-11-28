f24b4af57a1756138bd169cf3374b40f2fcc6c26
woto93 <albin.woto@gmail.com>
2019-11-27 22:20:16 -0300

    Bootstrap y estilos al primer componente

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,27 @@
 import React from "react";
 
 import confLogo from "../images/badge-header.svg";
+import "./styles/Badge.css";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className='Badge__section-name'>
+          <img className='Badge__avatar' src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
           <h1>
             Albin <br />
             Wotoszyn
           </h1>
         </div>
-        <div>
-          <p>Frontend Developer</p>
-          <p>@albin_w</p>
+        <div className='Badge__section-info'>
+          <h3>Frontend Developer</h3>
+          <div>@albin_w</div>
         </div>
-        <div>#PlatziConf</div>
+        <div className='Badge__footer'>#PlatziConf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css'
 
+import './global.css'
 import Badge from "./components/Badge";
+
 
 const container = document.getElementById("app");
 
