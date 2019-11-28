ead638fbf7018da69b16b32aa3a93ba2da7678eb
woto93 <albin.woto@gmail.com>
2019-11-28 17:06:57 -0300

    Agrego primeras propiedades a Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -10,18 +10,22 @@ class Badge extends React.Component {
         <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-        <div className='Badge__section-name'>
-          <img className='Badge__avatar' src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src={this.props.avatarUrl}
+            alt="Avatar"
+          />
           <h1>
-            Albin <br />
-            Wotoszyn
+            {this.props.firstName} <br />
+            {this.props.lastName}
           </h1>
         </div>
-        <div className='Badge__section-info'>
-          <h3>Frontend Developer</h3>
-          <div>@albin_w</div>
+        <div className="Badge__section-info">
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
-        <div className='Badge__footer'>#PlatziConf</div>
+        <div className="Badge__footer">#PlatziConf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
-import './global.css'
+import "./global.css";
 import Badge from "./components/Badge";
-
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Estanislao Albin"
+    lastName="Wotoszyn Grillo"
+    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+    jobTitle="Frontend Developer"
+    twitter="albin_w"
+  />,
+  container
+);
