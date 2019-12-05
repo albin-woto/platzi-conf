e534325959fb91cd4cdf7f5b22cbb91be4ae783c
woto93 <albin.woto@gmail.com>
2019-12-05 14:15:19 -0300

    Used react router to create a SPA

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"; 
 
 import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
@@ -54,9 +55,9 @@ class Badges extends React.Component {
         </div>
         <div className="Badges__container">
           <div className="Badges__buttons">
-            <a href="/badges/new" className="btn btn-primary">
+            <Link to="/badges/new" className="btn btn-primary">
               New Badge
-            </a>
+            </Link>
           </div>
           <div className="Badges__list">
             <div className="Badges__container">
