4f25a805036134cdffc16b7c19ef273210366564
woto93 <albin.woto@gmail.com>
2019-12-16 18:35:53 -0300

    Mejoro sitaxis

--- src/api.js
+++ src/api.js
@@ -24,8 +24,8 @@ async function callApi(endpoint, options = {}) {
 const api = {
   badges: {
     list() {
-      throw new Error('500: Server Error');
-      // return callApi('/badges');
+      // throw new Error('500: Server Error');
+      return callApi('/badges');
     },
     create(badge) {
       return callApi(`/badges`, {
